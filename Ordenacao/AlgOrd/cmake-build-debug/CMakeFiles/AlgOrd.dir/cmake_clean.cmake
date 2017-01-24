0f68c3a043a06b35b5338cb8d8bde3b20c70b94c
JonyLucas <jony.lucas.br13@gmail.com>
2017-01-23 21:47:01 -0300

    Add files via upload

---
+++ Ordenacao/AlgOrd/cmake-build-debug/CMakeFiles/AlgOrd.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AlgOrd.dir/main.cpp.obj"
+  "AlgOrd.pdb"
+  "AlgOrd.exe"
+  "AlgOrd.exe.manifest"
+  "libAlgOrd.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AlgOrd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
